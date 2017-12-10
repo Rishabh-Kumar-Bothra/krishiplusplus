9f0b5510ddab18a268e6d98f72eb862f4936e4b5
Rishabh Chauahn <rishabhc32@gmail.com>
2017-12-11 02:37:54 +0530

    changed local host port from 81 to 80

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/query', function (req, res) {
 
 })
 
-var server = app.listen(8081, function(){
+var server = app.listen(8080, function(){
     //var host = server.address().address
     var port = server.address().port;
     console.log("Example express app at port: %s", port);
