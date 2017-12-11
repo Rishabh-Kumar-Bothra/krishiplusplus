7bd0cd40230c08d0292c789b3337e5afc3c8f472
Rishabh Chauahn <rishabhc32@gmail.com>
2017-12-11 21:30:05 +0530

    fixed flood forecast not taking in consideration of inflow forecast

--- flood-cyclone.py
+++ flood-cyclone.py
@@ -12,15 +12,21 @@ except requests.exceptions.RequestException as error:
 
 soup = BeautifulSoup(r.text, 'lxml')
 sub_heading = soup.find_all('tr')
-level_forecast = sub_heading[2].text
 
-if(level_forecast.find('No') != -1):
-	print("No Flood Forecast")
+level_forecast = sub_heading[2].find_all('td')
+inflow_forecats = sub_heading[5].find_all('td')
 
+if ( len(level_forecast) > 1 ):
+	print( 'Flood (level) in state: ' + level_forecast[2].text )
 else:
-	print('Warning Flood: ' + sub_heading[2].text)
+	print('No level flood forecast')
 
-print('END1')
+if( len(inflow_forecats) > 1 ):
+	print( 'Flood (inflow) in state: ' + inflow_forecats[2].text )
+else:
+	print('No inflow flood forecats')
+
+print('ENDFLOOD')
 
 ''' CYCLONE'''
 
@@ -41,4 +47,4 @@ if ans.find('no') != -1:
 else:
 	print('Warning Cyclone : ' + ans)
 
-print('END2')
+print('ENDCYCLONE')
